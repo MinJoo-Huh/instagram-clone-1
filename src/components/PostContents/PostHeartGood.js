164783f8df83f6fa45e0c6a289124e3d82641b68
Minjoo Huh <huhminjoo@naver.com>
2024-12-12 06:50:49 +0900

    기능 구현
    - 게시물 이미지 넘김 버튼 활성화
    - 처음 이미지, 마지막 이미지 버튼 비활성화
    - 아이콘 상태에 따른 색상 변화

--- src/components/PostContents/PostHeartGood.js
+++ src/components/PostContents/PostHeartGood.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./PostHeartGood.module.css";
 
 const PostHeartGood = () => {
+  const [heartClick, setHeartClick] = useState(false);
+  const [bookmarkClick, setBookmarkClick] = useState(false);
+  const [isHeartHover, setIsHeartHover] = useState(false);
+  const [isBookmarkHover, setIsBookmarkHover] = useState(false);
+
+  // Mouse Click event
+  const heartClickHandler = () => {
+    setHeartClick((heartClick) => !heartClick);
+  };
+
+  const bookmarkClickHandler = () => {
+    setBookmarkClick((bookmarkClick) => !bookmarkClick);
+  };
+
+  // Mouse Hover or Out event
+  const isHeartHoverHandler = () => {
+    setIsHeartHover((isHeartHover) => !isHeartHover);
+  };
+
+  const isBookmarkHoverHandler = () => {
+    setIsBookmarkHover((isBookmarkHover) => !isBookmarkHover);
+  };
+
   return (
     <section className={classes.postHeartGood}>
       <div className={classes.leftIcons}>
         <img
           className={classes.heartIcon}
-          src={`${process.env.PUBLIC_URL}/images/heart.svg`}
+          // 1. click : true -> red heart
+          // 2. click : fasle & MouseOver -> heart
+          // 3. click : false & MouseOut -> gray heart
+          src={
+            heartClick
+              ? `${process.env.PUBLIC_URL}/images/redHeart.svg`
+              : isHeartHover
+              ? `${process.env.PUBLIC_URL}/images/grayHeart.svg`
+              : `${process.env.PUBLIC_URL}/images/heart.svg`
+          }
+          onClick={heartClickHandler}
+          onMouseOver={isHeartHoverHandler}
+          onMouseOut={isHeartHoverHandler}
         />
         <img
           className={classes.messageIcon}
@@ -21,7 +56,16 @@ const PostHeartGood = () => {
       <div className={classes.rightIcon}>
         <img
           className={classes.bookmarkIcon}
-          src={`${process.env.PUBLIC_URL}/images/bookmark.svg`}
+          src={
+            bookmarkClick
+              ? `${process.env.PUBLIC_URL}/images/blackBookmark.svg`
+              : isBookmarkHover
+              ? `${process.env.PUBLIC_URL}/images/grayBookmark.svg`
+              : `${process.env.PUBLIC_URL}/images/bookmark.svg`
+          }
+          onClick={bookmarkClickHandler}
+          onMouseOver={isBookmarkHoverHandler}
+          onMouseOut={isBookmarkHoverHandler}
         />
       </div>
     </section>
