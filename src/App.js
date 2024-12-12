d1431c2b3c5c5d68ed8b2fe791ab86d7c552eb33
bak yeong uk <yeonguk0517@gmail.com>
2024-12-13 02:35:37 +0900

    컴포넌트 합치기 & 구조 잡기

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import Carousel from './components/Carousel/Carousel';
 import Profile from './components/Profile/Profile';
 import PostImage from './components/PostContents/PostImage';
 import PostHeartGood from './components/PostContents/PostHeartGood';
+import PostContents from './components/PostContents/PostContents';
+import Sidebar from './components/Navigation/Sidebar';
 import './app.css';
 
 const App = () => {
   return (
     <div className='container'>
-      <nav></nav>
+      <nav>
+        <Sidebar />
+      </nav>
       <main>
         <div className='post-area'>
           <Carousel />
-          <PostImage />
-          <PostHeartGood />
+          {/* <PostImage />
+          <PostHeartGood /> */}
+          <PostContents />
         </div>
         <div className='recommendation-area'>
           <Profile />
--- src/components/Navigation/Post.js
+++ src/components/Navigation/Post.js
@@ -1,33 +1,25 @@
-import React from "react";
-import HeartIcon from "./HeartIcon";
-import "./Post.css";
+import React from 'react';
+import HeartIcon from './HeartIcon';
+import './Post.css';
 
 const Post = () => {
   return (
-    <div className="post">
+    <div className='post'>
       /* 게시물 헤더 */
-      <div className="post__header">
-        <img
-          src="https://via.placeholder.com/32"
-          alt="Profile"
-          className="post__avatar"
-        />
-        <span className="post__username">selra_z0921</span>
+      <div className='post__header'>
+        <img src='https://via.placeholder.com/32' alt='Profile' className='post__avatar' />
+        <span className='post__username'>selra_z0921</span>
       </div>
       /* 게시물 이미지 */
-      <img
-        src="https://via.placeholder.com/600x400"
-        alt="Post"
-        className="post__image"
-      />
+      <img src='https://via.placeholder.com/600x400' alt='Post' className='post__image' />
       /* 게시물 내용 */
-      <div className="post__content">
+      <div className='post__content'>
         <p>
           <strong>selra_z0921</strong> #뮤지컬 #오페라의유령 #벌써아쉽다..
         </p>
       </div>
       /* 게시물 하단 */
-      <div className="post__footer">
+      <div className='post__footer'>
         <p>댓글 보기</p>
         <HeartIcon />
       </div>
