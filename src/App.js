a1780c89d7c130f8ee3cabc1e8bfa43695ebb4b0
bak yeong uk <yeonguk0517@gmail.com>
2024-12-13 02:42:01 +0900

    주석 처리된 코드 제거

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import Carousel from './components/Carousel/Carousel';
 import Profile from './components/Profile/Profile';
-import PostImage from './components/PostContents/PostImage';
-import PostHeartGood from './components/PostContents/PostHeartGood';
 import PostContents from './components/PostContents/PostContents';
 import Sidebar from './components/Navigation/Sidebar';
 import './App.css';
@@ -15,8 +13,6 @@ const App = () => {
       <main>
         <div className='post-area'>
           <Carousel />
-          {/* <PostImage />
-          <PostHeartGood /> */}
           <PostContents />
         </div>
         <div className='recommendation-area'>
