8693588348064c96f818804b38efb1c0e75f25c7
bak yeong uk <yeonguk0517@gmail.com>
2024-12-12 23:16:15 +0900

    기본 구조 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import Carousel from './components/Carousel';
+import Profile from './components/Profile';
+import './App.css';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className='container'>
+      <nav></nav>
+      <main>
+        <div className='post-area'>
+          <Carousel />
+        </div>
+        <div className='recommendation-area'>
+          <Profile />
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default App;
